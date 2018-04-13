2df6b0b3ee7341d257f913dec7eacbde1d8b945f
Ronald Moesbergen <ronald.moesbergen@solvinity.com>
2018-04-13 22:22:01 +0200

    Kubus

---
+++ kubus/kubus.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(A6, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(A0, 1);
+  digitalWrite(13, 0);
+  digitalWrite(A6, 0);
+}
+
+
